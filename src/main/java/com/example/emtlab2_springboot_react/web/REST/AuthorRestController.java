96132c9c49cbdeca2dba8d868db60fb2c1ac1ccc
bojan-suklev <96276041+bojan-suklev@users.noreply.github.com>
2022-04-25 21:54:34 +0200

    After react deployment

--- src/main/java/com/example/emtlab2_springboot_react/web/REST/AuthorRestController.java
+++ src/main/java/com/example/emtlab2_springboot_react/web/REST/AuthorRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "https://emtlab2backendspring.herokuapp.com/")
+@CrossOrigin(origins = "https://emtlab2frontendreact.herokuapp.com")
 @RequestMapping("/api/author")
 public class AuthorRestController {
     private final AuthorService authorService;
